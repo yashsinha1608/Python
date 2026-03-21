3f3cdb3928d885d5bb859789c7bf04fd5dc1db25
YASH <yash1608.sinha@gmail.com>
2026-03-21 23:14:46 +0530

    s

--- p2.py
+++ p2.py
@@ -4,3 +4,13 @@ print(str1+str2)
 print(len(str1))
 str="Yash sinha is in KIIT"
 print(str[1:7])  #slicing 
+
+
+# NEGATIVE INDEX
+str3="APPLE"
+print(str3[-3:-1])   #A(-5)P(-4)P(-3)L(-2)E(-1)
+
+str4="i am a Coder"
+print(str4.endswith("er")) #return true if ends with er
+print(str4.capitalize()) #makes a new string and capatalize the first letter 
+print(str4.replace("a","z"))
