34a2fa1fc10904be679fc089910ab8d9d8481bef
YASH <yash1608.sinha@gmail.com>
2026-03-22 13:31:29 +0530

    s

--- sets.py
+++ sets.py
@@ -1,7 +1,17 @@
 #sets is unordered and duplicate values are ignored     
+#sets are mutabel 
+#but elements in set are immutable 
 
 nums={ 1,2,4,6,"yash","sinha"}
 print(nums)
 print(len(nums))
 
 empty_set1= set() #empty set is created 
+
+#methods in set 
+
+nums.add(23)
+nums.remove(6)
+print(nums)
+#nums.clear() #clears the entire set 
+nums.pop() #pop a random value
