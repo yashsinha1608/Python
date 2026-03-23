9222f96b70ba3a202b30e76ba742789d559c3a42
YASH <yash1608.sinha@gmail.com>
2026-03-23 23:06:49 +0530

    s

--- p6_loops.py
+++ p6_loops.py
@@ -1,17 +1,29 @@
 #while loop
 #while condition:
  #some work
-n=5
+"""n=5
 while n<7:
     print(n)
-    n+=1
+    n+=1"""
 
 #continue and break statement is also used 
 
 #for loop
-list = [2,5,1,7,1]
-for num in list:
+list = [2,5,1,7,1,12,54,21,1,2]
+"""for num in list:
     print(num)
 else:
     print("the iteration is done") # else will always be executed after the loop is completed 
 
+#range()"""
+
+#seq=range(5)
+
+"""print(seq[0])
+print(seq[2])
+print(range(5))"""
+
+#range(start,stop,incrinment)
+
+for el in range(2,7,2):
+    print(el)
