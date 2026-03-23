637f0cf0bf30a48ffd59fb23450aec7b041e5c4a
YASH <yash1608.sinha@gmail.com>
2026-03-23 22:30:14 +0530

    s

--- p6_loops.py
+++ p6_loops.py
@@ -10,8 +10,8 @@ while n<7:
 
 #for loop
 list = [2,5,1,7,1]
-
 for num in list:
     print(num)
-
+else:
+    print("the iteration is done") # else will always be executed after the loop is completed 
 
