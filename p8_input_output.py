361bbeed4399bea999aae7fc15606324c06a8b56
YASH <yash1608.sinha@gmail.com>
2026-03-24 00:15:13 +0530

    s

--- p8_input_output.py
+++ p8_input_output.py
@@ -1,7 +1,15 @@
 #f=open("file name","mode")  mode:- either read or write 
-f=open("demo.txt","r")
+"""f=open("demo.txt","r")
 data=f.read(6)
 data=f.readline() #reads one line at a time
 
 print(data)
-f.close()
+f.close()"""
+
+f=open("demo.txt","w") #writting a file
+f.write("This is a new inserted line")  #overwrites the entire file 
+
+f=open("demo.txt","r")
+print(f.read())
+
+
